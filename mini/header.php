0cefbd7ecb6ffd321c7afc9e1d3a23b34bdc759f
Angus Bodle <bodleangus@gmail.com>
2024-06-07 17:53:13 +1200

    06/06/2024

--- mini/header.php
+++ mini/header.php
@@ -1,16 +1,17 @@
-<nav class="navbar navbar-expand-lg py-3 px-3 pe-4 p-lg-0">
-  <div class="container-fluid p-lg-0 d-flex">
-
-    <div class="d-flex flex-column justify-content-between align-items-center left-nav-buttons fixed-top rounded-4 m-4 flex-shrink-1" style="max-width: fit-content;">
-
+<nav class="d-flex p-0">
+  <div class="container-fluid p-0">
+    <div class="nav-bar d-flex flex-column justify-content-between align-items-center left-nav-buttons fixed-top  m-0 m-lg-4 flex-shrink-1 col-12" style="">
+      
       <div class="d-flex flex-row g-0 w-100 align-items-center justify-content-between">
         <button class="menu-button border-0 p-0" type="button" data-bs-toggle="collapse" data-bs-target="#menuCollapse" aria-expanded="false" aria-controls="menuCollapse">
           <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16" id="menu-collapse-icon">
             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
           </svg>
         </button>
-        <p class="nav-logo px-3 d-flex align-items-center m-0">Tairāwhiti Uncovered.</p>
+        <p class="nav-logo px-0 px-lg-3 d-flex align-items-center m-0">Tairāwhiti Uncovered.</p>
+        <div class="mobile-header-spacer"></div>
       </div>
+      
       <div class="collapse menu-box-main flex-grow-1" id="menuCollapse">
         <div class="pt-3">
           <button type="button" class="menu-main-button py-4">
@@ -23,17 +24,7 @@
             </div>
             <div class="menu-main-button-rect"></div></button>
         </div>
-        <div class="">
-          <button type="button" class="menu-main-button py-4" id="newMarkerLink" data-bs-toggle="modal" data-bs-target="#createMarkerModal" >
-            <div class="d-flex flex-direction-row align-items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="me-2 bi bi-geo" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.3 1.3 0 0 0-.37.265.3.3 0 0 0-.057.09V14l.002.008.016.033a.6.6 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.6.6 0 0 0 .146-.15l.015-.033L12 14v-.004a.3.3 0 0 0-.057-.09 1.3 1.3 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465s-2.462-.172-3.34-.465c-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411"/>
-              </svg>
-              Create New Place
-            </div>
-            <div class="menu-main-button-rect"></div>
-          </button>
-        </div>
+        
         <div class="th">
 
         </div>
